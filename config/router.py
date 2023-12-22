a13728964945c964586c648720471ae091dde40d
AkbarYusupov2003 <akbarbek.yusupov@gmail.com>
2023-12-22 11:41:04 +0500

    update

--- config/router.py
+++ config/router.py
@@ -1,1 +1,25 @@
-# TODO
+class DBRouter:
+    route_model_names = ("Subscription", "Account", "IntermediateSubscription")
+
+    def db_for_read(self, model, **hints):
+        if model._meta.object_name in self.route_model_names:
+            return "splay"
+        return None
+
+    def db_for_write(self, model, **hints):
+        if model._meta.object_name in self.route_model_names:
+            return "splay"
+        return None
+
+    def allow_relation(self, obj1, obj2, **hints):
+        return True if obj1._meta.object_name in self.route_model_names or obj2._meta.object_name in self.route_model_names else None
+
+    def allow_migrate(self, db, app_label, model_name=None, **hints):
+        if db == "splay":
+            if model_name in self.route_model_names:
+                return True
+            return False
+        else:
+            if model_name in self.route_model_names:
+                return False
+            return True
