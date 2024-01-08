5f41a8eea401a1bc65b855b903f5d95e29f2f1f8
AkbarYusupov2003 <akbarbek.yusupov@gmail.com>
2024-01-08 14:37:58 +0500

    update

--- payze/views.py
+++ payze/views.py
@@ -17,31 +17,19 @@ class ProductCreateAPIView(View):
 
     def get(self, request, *args, **kwargs):
         url = "https://payze.io/v2/api/product"
+
         body = {
-            "name": "Подписка активация",
-            "description": "Подписка активация",
-            "imageUrl": "https://payze.io?imageId=12",
+            "name": "Активация",
+            "description": "Активация",
+            "imageUrl": "",
             "price": "35000",
             "currency": "UZS",
             "occurrenceType": "Day",
-            "occurrenceNumber": "1",
-            "occurrenceDuration": "10",
-            "freeTrial": 5,
-            "numberOfFailedRetry": 3
+            "occurrenceNumber": "30",
+            "occurrenceDuration": "1",
+            "freeTrial": 0,
+            "numberOfFailedRetry": 1
         }
         response = extractor.get_data(url, body)
         print("RESPONSE", response)
-        # {
-        #     "name": "Product Name",
-        #     "description": "Product Description",
-        #     "imageUrl": "https://payze.io?imageId=12",
-        #     "price": "50",
-        #     "currency": "GEL",
-        #     "occurrenceType": "Day",
-        #     "occurrenceNumber": "1",
-        #     "occurrenceDuration": "10",
-        #     "freeTrial": 5,
-        #     "numberOfFailedRetry": 3
-        # }
-
         return HttpResponse("Ok")
