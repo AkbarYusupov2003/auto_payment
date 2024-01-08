196d425ca2eef8305f6bdd7570741f59f4453cd2
AkbarYusupov2003 <akbarbek.yusupov@gmail.com>
2024-01-08 13:07:21 +0500

    update

--- payment/utils/tokens.py
+++ payment/utils/tokens.py
@@ -3,4 +3,4 @@ from django.conf import settings
 
 
 def get_data_from_token(token):
-    return jwt.decode(token[7:], key=settings.SPLAY_JWT_KEY, algorithms=("HS256",), options={"verify_signature": True})
+    return jwt.decode(token.replace("Bearer", ""), key=settings.SPLAY_JWT_KEY, algorithms=("HS256",), options={"verify_signature": True})
